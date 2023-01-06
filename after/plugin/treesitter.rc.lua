dbda891f866be2908de32b2475671e43d1738c6b
1617003785@qq.com <swaylivrx@foxmail.com>
2023-01-06 20:15:33 +0800

    update this

--- after/plugin/treesitter.rc.lua
+++ after/plugin/treesitter.rc.lua
@@ -2,7 +2,7 @@ require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
   ensure_installed = "all",
   sync_install = false,
-  auto_install = true,
+  auto_install = false,
   ignore_install = {},
   highlight = {
     enable = true,
