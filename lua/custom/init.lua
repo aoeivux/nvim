cd870debc2c655c910372985d5256a999b331fc3
Swaylivrx <1617003785@qq.com>
2023-01-18 13:17:28 +0800

    close default lcd %:p:h replace into noacd.

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,12 +1,13 @@
 local autocmd = vim.api.nvim_create_autocmd
 
-autocmd("BufEnter", {
+--autochdir
+--[[ autocmd("BufEnter", {
    callback = function()
       if vim.api.nvim_buf_get_option(0, "buftype") ~= "terminal" then
          vim.cmd "lcd %:p:h"
       end
    end,
-})
+}) ]]
 
 
 -- 打开时自动定位到上次关闭位置
@@ -14,6 +15,12 @@ autocmd(
   { "BufReadPost" },
   {pattern = "*", command = [[if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]] }
 )
+
+-- auto change dir noautochdir
+vim.api.nvim_command('set noacd')
+vim.cmd [[
+autocmd VimEnter * set noacd
+]]
 
 
 
