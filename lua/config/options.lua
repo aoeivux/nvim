e10c6f27b4af9c7bddbe45e6763e6bb164b0b23d
Yongxing Zhu <1617003785@qq.com>
2024-01-06 21:48:22 +0800

    Update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -36,7 +36,7 @@ vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
 vim.opt.splitbelow = true -- Put new windows below current
 vim.opt.splitright = true -- Put new windows right of current
 vim.opt.splitkeep = "cursor"
-vim.opt.mouse = ""
+vim.opt.mouse = "a"
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
