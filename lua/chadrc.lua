27bbac97756ba73bb585cc5cf90bef438e4f4359
Yongxing Zhu <1617003785@qq.com>
2025-03-22 16:45:58 +0800

    Update chadrc.lua

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,73 +1,29 @@
 -- This file needs to have same structure as nvconfig.lua 
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 -- Please read that file to know all available options :( 
----@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-    theme = "solarized_osaka",
-    -- hl_override = {},
-    -- hl_add = {},
-    -- below default
-    -- integrations = {},
-    -- changed_themes = {},
+    theme = "gruvbox",
     transparency = false,
-    -- theme_toggle = { "onedark", "one_light" },
-}
-
-M.ui = {
-
-    cmp = {
-        icons = true,
-        lspkind_text = true,
-        style = "atom" -- default/flat_light/flat_dark/atom/atom_colored
-    },
-
-    telescope = {
-        style = "borderless"
-    }, -- borderless / bordered
-
-    statusline = {
-        theme = "default", -- default/vscode/vscode_colored/minimal
-        -- default/round/block/arrow separators work only for default statusline theme
-        -- round and block will work for minimal theme only
-        separator_style = "default",
-        order = nil,
-        modules = nil
-    },
-
-    -- lazyload it when there are 1+ buffers
-    tabufline = {
-        enabled = true,
-        lazyload = true,
-        order = {"treeOffset", "buffers", "tabs", "btns"},
-        modules = nil
-    },
-
 }
 
 M.nvdash = {
         load_on_startup = true,
-        header = {"⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕",
-                  "⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕",
-                  "⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱",
-                  "⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀",
-                  "⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗",
-                  "⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕",
-                  "⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵",
-                  "⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿",
-                  "⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿",
-                  "⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁", "",
-                  "Hello, master!"},
-        -- stylua: ignore
-        buttons = {{txt = "  Find File", key = "Spc f f", cmd = "Telescope find_files"},
-                   {txt = "  Bookmarks", key = "Spc m a", cmd = "Telescope marks"},
-                   {txt = "  Live Grep", key = "Spc f w", cmd = "Telescope live_grep"},
-                   {txt = "󰈚  Recent Files", key = "Spc f r", cmd = "Telescope oldfiles"},
-                   {txt = "  Themes", key = "Spc t h", "Telescope themes"},
-                   {txt = "  Mappings", key = "Spc c h", cmd = "NvCheatsheet"}}
+header = {
+        "",
+        "⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕",
+        "⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕",
+        "⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱",
+        "⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀",
+        "⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗",
+        "⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕",
+        "⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵",
+        "⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿",
+        "⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿",
+        "⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁"
+    },
 }
-
 
 M.term = {
     winopts = {
@@ -97,11 +53,6 @@ M.lsp = {
 M.cheatsheet = {
     theme = "grid", -- simple/grid
     excluded_groups = {"terminal (t)", "autopairs", "Nvim", "Opens"} -- can add group name or with mode
-}
-
-M.mason = {
-    cmd = true,
-    pkgs = {"typescript-language-server", "pyright"}
 }
 
 return M
